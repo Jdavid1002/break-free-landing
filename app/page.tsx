6d0b70b9c9f7805bcef4eb5a2c054e15cc171548
Maiky33 <maicolbarrerag@gmail.com>
2023-02-22 19:21:04 -0500

    Banner add

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
-import { Open_Sans } from '@next/font/google'
-import Navbar from '@/components/Navbar'
-import Banner from '@/components/Banner'
-import { NextFont } from '@next/font'
+import {Open_Sans} from "@next/font/google";
+import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
+import { NextFont } from "@next/font";
 
-const primaryFont : NextFont = Open_Sans({
-  subsets: ['latin'],
-  weight: '400',
-})
+const primaryFont: NextFont = Open_Sans({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Home() {
   return (
     <main>
-      DEVELOPMENT
-
-      <Navbar 
-        primaryFont={primaryFont}
-      />
-
-      <Banner 
-        primaryFont={primaryFont}
-      />
-
-      <h2 className={primaryFont.className}>
-        HOLA
-      </h2>
-
+      {/*DEVELOPMENT*/}
+      <Navbar primaryFont={primaryFont} />
+      <Banner primaryFont={primaryFont}/>
     </main>
-  )
+  );
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,22 +1,69 @@
-'use client'
+"use client";
 
 import { NextFont } from "@next/font";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export interface IBanner {
-  primaryFont : NextFont
+import {Pagination, Autoplay } from 'swiper';
+import "swiper/swiper.min.css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import Image from 'next/image'
+import Styles from './Banner.module.css'
+import SlideOne from './images/SlideOne.jpg'
+import SlideTwo from './images/SlideTwo.jpg'
+import SlideThree from './images/SlideThree.jpg'
+import SlideFour from './images/SlideFour.jpg'
+
+
+interface IBanner {
+  primaryFont: NextFont;
+}
+interface ISlider { 
+  image: any
+  id: number
 }
 
-const Banner = (props : IBanner) => {
+const SliderItem = [
+  {
+    image: SlideOne,
+    id: 1,
+  },
+  {
+    image: SlideTwo,
+    id: 2,
+  },{
+    image: SlideThree,
+    id: 3,
+  },
+  {
+    image: SlideFour,
+    id: 4,
+  }
+];
 
-  const { primaryFont } = props
+
+const Banner = (props: IBanner) => {
+  const { primaryFont } = props;
 
   return (
-    <div>
-      <h2 className={primaryFont.className}>
-        Banner 
-      </h2>
+    <div className={`${primaryFont.className} ${Styles.ContainerSlider}`}>
+      <Swiper
+        modules={[Pagination,Autoplay]}
+        className={Styles.Slider}
+        slidesPerView={1}
+        autoplay={{delay: 3000}}
+        pagination={{clickable: true}}
+      >
+        {SliderItem.map((item:ISlider) => (
+          <SwiperSlide className={Styles.SwiperSlide} key={item.id}>
+            <Image className={Styles.image} alt='images' src={item.image}/>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
-}
- 
+};
+
 export default Banner;
